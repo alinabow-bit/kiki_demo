90449b6c517b8dfaf77196a413851fd644dd553e
“hhy <alinabow@163.com>
2021-07-15 23:07:07 +0800

    add pom,testng,src

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,9 @@
+package testcases;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void loginSucess(){
+        System.out.println("登录成功");
+    }
+}
---
+++ src/test/java/testcases/RegisterTest.java
@@ -0,0 +1,9 @@
+package testcases;
+import org.testng.annotations.Test;
+
+@Test
+public class RegisterTest {
+    public void main() {
+        System.out.println("注册成功");
+    }
+}
